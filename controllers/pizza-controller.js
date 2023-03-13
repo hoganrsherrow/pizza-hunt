89bc9b006434aa5d9f72698dfb8f13e5eefe9fb8
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2023-03-13 13:13:31 -0500

    added comment-model, routing, and added frontend fetch

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,12 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -14,6 +20,10 @@ const pizzaController = {
     // get one pizza by _id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
             .then(dbPizzaData => {
                 // if no pizza is found with supplied id
                 if(!dbPizzaData) {
